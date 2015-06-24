dbf9f5adc9c6fba616c9f181eb94bd76ad00af7c
Saurav Kothari <sauravkothari2@gmail.com>
2015-06-24 19:52:09 +0530

    basic style elements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,12 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params)
-  	@user.save
-    redirect_to user_path(@user)
+    @user = User.new(user_params)
+    if (@user.save)
+      redirect_to user_path(@user)
+    else
+      render "new"
+    end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   has_many :attachments
   has_secure_password
 
+  validates :username, uniqueness: true
   validates :password, confirmation: true, length: { minimum: 6 }, allow_nil: true
 end
