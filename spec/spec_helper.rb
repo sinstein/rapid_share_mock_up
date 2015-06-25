767a65eed7a36f23b9d43245332d825f50a7b2bc
Saurav Kothari <sauravkothari2@gmail.com>
2015-06-25 12:29:02 +0530

    User testing

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+#require 'active_record'
+#require_relative "../app/models/user.rb"
+#require_relative "../app/models/attachment.rb"
+
+#ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.color = true
+end
+
+
